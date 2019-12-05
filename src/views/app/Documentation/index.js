c55925201c11f318d171a08f3b78378f5ef6907b
SkyTradeInc <38687801+SkyTradeInc@users.noreply.github.com>
2019-12-05 11:17:32 +1100

    add more methods

--- src/views/app/Documentation/index.js
+++ src/views/app/Documentation/index.js
@@ -31,22 +31,205 @@ const methods = [
   {
     name: 'eth_call',
     description: 'Executes a new message call immediately without creating a transaction on the block chain.',
-    params: []
+    params: [
+      {
+        name: 'Transaction Call Object',
+        description: 'integer of a block number, or the string "earliest", "latest" or "pending"',
+        types: ['string', 'number'],
+        required: true,
+        subParams: [
+          {
+            name: 'from',
+            description: 'The address the transaction is sent from',
+            types: ['string'],
+            required: false
+          },
+          {
+            name: 'to',
+            description: 'The address the transaction is directed to',
+            types: ['string'],
+            required: true
+          },
+          {
+            name: 'gas',
+            description: 'Integer of the gas provided for the transaction execution. eth_call consumes zero gas, but this parameter may be needed by some executions.',
+            types: ['string', 'number'],
+            required: false
+          },
+          {
+            name: 'gasPrice',
+            description: 'Integer of the gasPrice used for each paid gas.',
+            types: ['string', 'number'],
+            required: false
+          },
+          ,
+          {
+            name: 'value',
+            description: 'Integer of the value sent with this transaction.',
+            types: ['string', 'number'],
+            required: false
+          },
+          ,
+          {
+            name: 'data',
+            description: 'Hash of the method signature and encoded parameters. For details see Ethereum Contract ABI',
+            types: ['string', 'number'],
+            required: false
+          },
+        ]
+      },
+      {
+        name: 'Block Paramater',
+        description: 'integer block number, or the string "latest", "earliest" or "pending"',
+        types: ['string', 'number'],
+        required: true
+      }
+    ]
+  },
+  {
+    name: 'eth_estimateGas',
+    description: 'Generates and returns an estimate of how much gas is necessary to allow the transaction to complete. The transaction will not be added to the blockchain. Note that the estimate may be significantly more than the amount of gas actually used by the transaction, for a variety of reasons including EVM mechanics and node performance.',
+    params: [
+      {
+        name: 'Transaction Call Object',
+        description: 'integer of a block number, or the string "earliest", "latest" or "pending"',
+        types: ['string', 'number'],
+        required: true,
+        subParams: [
+          {
+            name: 'from',
+            description: 'The address the transaction is sent from',
+            types: ['string'],
+            required: false
+          },
+          {
+            name: 'to',
+            description: 'The address the transaction is directed to',
+            types: ['string'],
+            required: true
+          },
+          {
+            name: 'gas',
+            description: 'Integer of the gas provided for the transaction execution. eth_call consumes zero gas, but this parameter may be needed by some executions.',
+            types: ['string', 'number'],
+            required: false
+          },
+          {
+            name: 'gasPrice',
+            description: 'Integer of the gasPrice used for each paid gas.',
+            types: ['string', 'number'],
+            required: false
+          },
+          ,
+          {
+            name: 'value',
+            description: 'Integer of the value sent with this transaction.',
+            types: ['string', 'number'],
+            required: false
+          },
+          ,
+          {
+            name: 'data',
+            description: 'Hash of the method signature and encoded parameters. For details see Ethereum Contract ABI',
+            types: ['string', 'number'],
+            required: false
+          },
+        ]
+      }
+    ]
+  },
+  {
+    name: 'eth_getBalance',
+    description: 'Returns the balance of the account of given address.',
+    params: [
+      {
+        name: 'Address',
+        description: 'a string representing the address (20 bytes) to check for balance',
+        types: ['string'],
+        required: true
+      },
+      {
+        name: 'Block Paramater',
+        description: 'integer of a block number, or the string "earliest", "latest" or "pending"',
+        types: ['string', 'number'],
+        required: true
+      }
+    ]
+  },
+  {
+    name: 'eth_getBlockByHash',
+    description: 'Returns information about a block by hash.',
+    params: [
+      {
+        name: 'Block Hash',
+        description: 'a string representing the hash (32 bytes) of a block',
+        types: ['string'],
+        required: true
+      },
+      {
+        name: 'Show Transaction Details Flag',
+        description: 'If true it returns the full transaction objects, if false only the hashes of the transactions.',
+        types: ['boolean'],
+        required: true
+      }
+    ]
   },
   {
     name: 'eth_getBlockByNumber',
     description: 'Returns information about a block by block number.',
     params: [
       {
-        name: 'QUANTITY|TAG',
+        name: 'Block Paramater',
         description: 'integer of a block number, or the string "earliest", "latest" or "pending"',
         types: ['string', 'number'],
         required: true
       },
       {
-        name: 'Boolean',
+        name: 'Show Transaction Details Flag',
         description: 'If true it returns the full transaction objects, if false only the hashes of the transactions.',
         types: ['boolean'],
+        required: true
+      }
+    ]
+  },
+  {
+    name: 'eth_getBlockTransactionCountByHash',
+    description: 'Returns the number of transactions in the block with the given block hash.',
+    params: [
+      {
+        name: 'Block Hash',
+        description: 'a string representing the hash (32 bytes) of a block',
+        types: ['string'],
+        required: true
+      }
+    ]
+  },
+  {
+    name: 'eth_getBlockTransactionCountByNumber',
+    description: 'Returns the number of transactions in the block with the given block number.',
+    params: [
+      {
+        name: 'Block Paramater',
+        description: 'integer of a block number, or the string "earliest", "latest" or "pending"',
+        types: ['string', 'number'],
+        required: true
+      }
+    ]
+  },
+  {
+    name: 'eth_getCode',
+    description: 'Returns the compiled smart contract code, if any, at a given address.',
+    params: [
+      {
+        name: 'Address',
+        description: 'a string representing the address (20 bytes) of the code',
+        types: ['string'],
+        required: true
+      },
+      {
+        name: 'Block Paramater',
+        description: 'integer of a block number, or the string "earliest", "latest" or "pending"',
+        types: ['string', 'number'],
         required: true
       }
     ]
@@ -78,7 +261,7 @@ export default class Documentation extends Component {
       },
       selectedMethod: false,
       response: '',
-      apiKey: '601b14ce1ed44770d8376c90360deae18a8d4dbf41264748924f86ae76664638'
+      apiKey: '322970fbb2bf9127cfa1613981844fa6b8dd1433e9122edf833aeb31613533c7'
     };
   }
 
@@ -173,7 +356,7 @@ export default class Documentation extends Component {
                 <br/><br/>
                 <h3> Response </h3>
                 <Highlight language={"json"}>
-                {this.state.response === '' ? '' : this.state.response}
+                {this.state.response === '' ? '\n' : this.state.response}
                 </Highlight>
               </>
             : null}
